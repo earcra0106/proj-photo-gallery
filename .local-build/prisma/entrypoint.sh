1ae69d3671b5a96fd76631056c5b2d6688989be0
earcra0106 <earphonecrasher@gmail.com>
2026-03-23 17:54:44 +0900

    ローカルビルド定義を調整

--- .local-build/prisma/entrypoint.sh
+++ .local-build/prisma/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 set -eu
 
-echo "[prisma] Run Migrations : ${DATABASE_URL}"
+ESC=$(printf '\033')
+BIGREEN="${ESC}[1;92m"
+
+echo "${BIGREEN}Migrationを実行中 : ${DATABASE_URL}${ESC}[m"
 pnpm --filter @repo/prisma migrate:prod
 
 if [ "${ENABLE_DB_SEED:-false}" = "true" ]; then
-  echo "[prisma] Run Seeding"
+  echo "${BIGREEN}初期データの投入を実行中 : ${DATABASE_URL}${ESC}[m"
   pnpm --filter @repo/prisma seed
 fi
 
-echo "[prisma] Start Prisma Studio"
-exec pnpm --filter @repo/prisma studio --browser none --port 5555
+if [ "${ENABLE_PRISMA_STUDIO:-false}" = "true" ]; then
+  echo "${BIGREEN}Prisma Studioを起動中${ESC}[m"
+  exec pnpm --filter @repo/prisma studio --browser none --port ${PRISMA_STUDIO_PORT:-5555}
+else
+  echo "${BIGREEN}Prisma Studioを起動する場合は、環境変数 ENABLE_PRISMA_STUDIO をtrueに設定してください。${ESC}[m"
+  echo "${BIGREEN}コンテナを終了します。${ESC}[m"
+fi
