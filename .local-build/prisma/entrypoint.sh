20598818f947cd2cf0cd174dc1f6f87ac109eedb
earcra0106 <earphonecrasher@gmail.com>
2026-03-30 13:08:51 +0900

    キャッシュ戦略改善

--- .local-build/prisma/entrypoint.sh
+++ .local-build/prisma/entrypoint.sh
@@ -5,16 +5,16 @@ ESC=$(printf '\033')
 BIGREEN="${ESC}[1;92m"
 
 echo "${BIGREEN}Migrationを実行中 : ${DATABASE_URL}${ESC}[m"
-pnpm --filter @repo/prisma migrate:prod
+pnpm --filter @repo/prisma db:migrate:prod
 
 if [ "${ENABLE_DB_SEED:-false}" = "true" ]; then
   echo "${BIGREEN}初期データの投入を実行中 : ${DATABASE_URL}${ESC}[m"
-  pnpm --filter @repo/prisma seed
+  pnpm --filter @repo/prisma db:seed
 fi
 
 if [ "${ENABLE_PRISMA_STUDIO:-false}" = "true" ]; then
   echo "${BIGREEN}Prisma Studioを起動中${ESC}[m"
-  exec pnpm --filter @repo/prisma studio --browser none --port ${PRISMA_STUDIO_PORT:-5555}
+  exec pnpm --filter @repo/prisma db:studio --browser none --port ${PRISMA_STUDIO_PORT:-5555}
 else
   echo "${BIGREEN}Prisma Studioを起動する場合は、環境変数 ENABLE_PRISMA_STUDIO をtrueに設定してください。${ESC}[m"
   echo "${BIGREEN}コンテナを終了します。${ESC}[m"
